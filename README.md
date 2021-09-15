7bb8ec74c186885dfbdc4a057152da54d320fae4
ccamrobertson <cameron@airtime.network>
2021-09-15 06:27:02 -0600

    minor readme changes.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# $CITIZEN ERC20
+# $CITIZEN
 
 ## Alpha
 
-Contract used for 500 KONG Land Alpha $CITIZEN tokens deployed at 0x77f0cc420dea0ae726db6bef1460a4b69176a8ea. Learn more at https://kong.land/.
+Contracts used for 500 KONG Land Alpha $CITIZEN ERC-20 tokens deployed at 0x77f0cc420dea0ae726db6bef1460a4b69176a8ea and KONG Land ERC-721 tokens deployed at 0x355929193308e157760824ba860390924d77fab9. Minting for the ERC-721 by burning the ERC-20 via BurnMintCitizen.sol. 
+
+Learn more at https://kong.land/.
 
 ## Test
 
--- scripts/deploy-erc721.js
+++ scripts/deploy-erc721.js
@@ -1,9 +1,9 @@
 const keccak256 = require('keccak256');
 
-// $CITIZEN ERC20 MAINNET: 0x77f0cc420dea0ae726db6bef1460a4b69176a8ea
-// $CITIZEN ERC20 ROPSTEN: 0x4e4c7051ecce3985403be5c551c55b716ddbf2ab
+const CITIZEN_ERC20_MAINNET = "0x77f0cc420dea0ae726db6bef1460a4b69176a8ea"
+const CITIZEN_ERC20_ROPSTEN = "0x4e4c7051ecce3985403be5c551c55b716ddbf2ab"
 
-var CitizenERC20Address = "0x77f0cc420dea0ae726db6bef1460a4b69176a8ea";
+var CitizenERC20Address = CITIZEN_ERC20_MAINNET;
 
 async function main() {
   const [deployer] = await ethers.getSigners();
@@ -12,15 +12,14 @@ async function main() {
 
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  // const CitizenERC20Token = await ethers.getContractFactory("CitizenERC20");
-  const CitizenERC721Token = await ethers.getContractFactory("CitizenERC721");
-  const BurnContract = await ethers.getContractFactory("BurnMintCitizen");
-
-  // Deploy the base contracts.
-
   // UNCOMMENT BELOW TO DEPLOY FRESH ERC20.
+  // const CitizenERC20Token = await ethers.getContractFactory("CitizenERC20");  
   // CitizenERC20 = await CitizenERC20Token.deploy();
   // CitizenERC20Address = CitizenERC20.address;
+
+  // Deploy the base contracts.
+  const CitizenERC721Token = await ethers.getContractFactory("CitizenERC721");
+  const BurnContract = await ethers.getContractFactory("BurnMintCitizen");
 
   CitizenERC721Proxy = await upgrades.deployProxy(CitizenERC721Token);
   BurnMintCitizen = await BurnContract.deploy(CitizenERC20Address, CitizenERC721Proxy.address);
