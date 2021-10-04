a93b9c84675ebfc688ddcb6fe8848f8c9b625c08
John Letey <johnletey@gmail.com>
2021-10-04 20:05:41 +0100

    feat: caller claims for a specific `tokenId`

--- contracts/CitizenENSRegistrar.sol
+++ contracts/CitizenENSRegistrar.sol
@@ -2,35 +2,54 @@
 pragma solidity ^0.8.4;
 
 import '@ensdomains/ens-contracts/contracts/registry/ENS.sol';
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 
 contract CitizenENSRegistrar {
     
     ENS public immutable _registry;
+    ERC721 public immutable _citizen;
 
     string public _rootName;  // citizen.eth
     bytes32 public _rootNode; // namehash(citizen.eth)
 
-    constructor(ENS registry, string memory rootName, bytes32 rootNode) {
+    // Mapping from token ID to subdomain label
+    mapping(uint256 => bytes32) public _labels;
+
+    constructor(ENS registry, ERC721 citizen, string memory rootName, bytes32 rootNode) {
 
         _registry = registry;
+        _citizen = citizen;
 
         _rootName = rootName;
         _rootNode = rootNode;
 
     }
 
-    function claim(string calldata label) public {
+    function claim(uint256 tokenId, string calldata label) public {
 
+        // Check that sender owns the supplied tokenId.
+        require(_citizen.ownerOf(tokenId) == msg.sender, "Caller must own the supplied tokenId.");
+
+        // Check that a subdomain hasn't already been claimed for this tokenId.
+        require(_labels[tokenId] == bytes32(0), "Caller has already claimed a subdomain.");
+
+        // Encode the supplied label.
         bytes32 labelNode = keccak256(abi.encodePacked(label));
         bytes32 node = keccak256(abi.encodePacked(_rootNode, labelNode));
 
-        // Make sure label hasn't been claimed.
-        require(_registry.owner(node) == address(0));
+        // Make sure the label hasn't been claimed.
+        require(_registry.owner(node) == address(0), "The supplied label has already been claimed.");
 
+        // Create the subdomain.
         _registry.setSubnodeOwner(_rootNode, labelNode, msg.sender);
+        _labels[tokenId] = labelNode;
 
         // TODO: Resolver?
 
     }
+
+    // TODO: Function for updating your subdomain.
+
+    // TODO: Admin function when user transfers their NFT.
 
 }
