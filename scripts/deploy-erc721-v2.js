0dcb66dd723c491aaab41901f8489c7f1211e13d
ccamrobertson <cameron@airtime.network>
2021-10-29 14:38:17 -0600

    adding in role setting for reveal.

--- scripts/deploy-erc721-v2.js
+++ scripts/deploy-erc721-v2.js
@@ -4,10 +4,11 @@ const keccak256 = require('keccak256');
 const CITIZEN_ERC721_MAINNET = "0x355929193308e157760824ba860390924d77fab9";
 const CITIZEN_ERC721_ROPSTEN = "0xd431b82796865F396e28d0f56b6a3d6626268A6e";
 
-// NOTE: to test on Ropsten, you may need to register the .test domain via ENS Domains
+// WARNING: to test on Ropsten, it is recommended to first get a fresh .test domain using the deployer account.
 const CITIZEN_ENS_MAINNET = "citizen.eth"
-const CITIZEN_ENS_ROPSTEN = "kongland.test"
+const CITIZEN_ENS_ROPSTEN = "kong.test"
 
+// Set your network here.
 var CitizenERC721Address = CITIZEN_ERC721_ROPSTEN;
 var CitizenENS = CITIZEN_ENS_ROPSTEN;
 
@@ -47,6 +48,9 @@ const main = async () => {
   // Deploy revealCitizen.sol.
   const RevealCitizen = await ethers.getContractFactory("RevealCitizen");
   const reveal = await RevealCitizen.deploy(CitizenERC721Address);
+
+  // Add a role granding device capability to RevealCitizen.
+  await nft.grantRole(keccak256('DEVICE_ROLE'), RevealCitizen.address);  
 
   // TODO: set oracle address.
 
